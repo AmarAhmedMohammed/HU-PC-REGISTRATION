b4f1bf9b8e9c96e82c33b33fa01791dcab91e139
Amar Ahmed <amarselmansudeys@gmail.com>
2025-11-28 09:42:18 -0900

    system update

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -20,9 +20,9 @@ import emailjs from 'emailjs-com';
 /* --------------------------------------------------------------
    REPLACE THESE WITH YOUR EMAILJS CREDENTIALS
    -------------------------------------------------------------- */
-const SERVICE_ID = "service_0nbyj0r";
-const TEMPLATE_ID = "template_fv6aubg";
-const PUBLIC_KEY = "F33pfI-zsdDEz0gy5";
+const SERVICE_ID = import.meta.env.VITE_EMAILJS_SERVICE_ID;
+const TEMPLATE_ID = import.meta.env.VITE_EMAILJS_TEMPLATE_ID;
+const PUBLIC_KEY = import.meta.env.VITE_EMAILJS_PUBLIC_KEY;
 /* -------------------------------------------------------------- */
 
 const Register = () => {
